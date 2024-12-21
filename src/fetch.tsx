5db51c8650d9142019aafa4fd6a28ad81e9db369
pablo-aviles-prieto <pablo.aviles.prieto@gmail.com>
2024-12-21 18:08:56 +0100

    Add codegen for TS support

--- src/fetch.tsx
+++ src/fetch.tsx
@@ -1,9 +1,11 @@
-import { useQuery, gql } from '@apollo/client';
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { graphql } from './graphql';
+import { GetImagesQuery, GetImagesQueryVariables } from './graphql/graphql';
+import { execute } from './graphql/execute';
 
-const GET_IMAGES = gql`
-  query GetImages($after: String) {
-    images(first: 23, after: $after) {
+const getImagesQuery = graphql(`
+  query GetImages($after: String, $first: Int = 20) {
+    images(first: $first, after: $after) {
       nodes {
         id
         title
@@ -20,48 +22,20 @@ const GET_IMAGES = gql`
       }
     }
   }
-`;
+`);
 
 export const FetchData = () => {
-  const { data, loading, error, fetchMore } = useQuery(GET_IMAGES);
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [images, setImages] = useState<any[]>([]);
-  const [hasNextPage, setHasNextPage] = useState<boolean>(false);
-  const [nextCursor, setNextCursor] = useState<string>('');
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      // const variables: GetImagesQueryVariables = undefined;
+      return execute<GetImagesQuery, GetImagesQueryVariables>(getImagesQuery);
+    },
+  });
 
-  useEffect(() => {
-    if (data?.images?.nodes) {
-      setImages(prev => [...prev, ...data.images.nodes]);
-      setHasNextPage(data.images.pageInfo.hasNextPage);
-      setNextCursor(data.images.pageInfo.endCursor);
-    }
-  }, [data]);
+  console.log('data', data);
+  console.log('error', error);
+  console.log('isLoading', isLoading);
 
-  const loadMore = async () => {
-    if (hasNextPage) {
-      const res = await fetchMore({
-        variables: { after: nextCursor },
-      });
-      if (res?.data?.images?.nodes) {
-        setImages(prev => [...prev, ...res.data.images.nodes]);
-        setHasNextPage(res.data.images.pageInfo.hasNextPage);
-        setNextCursor(res.data.images.pageInfo.endCursor);
-      }
-    }
-  };
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
-  return (
-    <div>
-      {images.map(img => (
-        <div key={img.id}>
-          <h3>{img.title}</h3>
-          {/* <img src={img.picture} alt={img.title} /> */}
-        </div>
-      ))}
-      {hasNextPage && <button onClick={loadMore}>Load More</button>}
-    </div>
-  );
+  return <div>Check</div>;
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client';
-
-const client = new ApolloClient({
-  link: new HttpLink({ uri: 'https://sandbox-api-test.samyroad.com/graphql' }),
-  cache: new InMemoryCache(),
-});
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ApolloProvider client={client}>
+    <QueryClientProvider client={queryClient}>
       <App />
-    </ApolloProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
