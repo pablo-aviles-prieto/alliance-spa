cd8bffa9e55b4cfa86d4babe159e2ccf882497fd
pablo-aviles-prieto <pablo.aviles.prieto@gmail.com>
2024-12-22 12:12:45 +0100

    Add tailwind

--- src/fetch.tsx
+++ src/fetch.tsx
@@ -1,22 +1,22 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
-import GraphqlRepository from './services/graphql-repository';
+import GraphqlRepository from '@/services/graphql-repository';
 
 export const FetchData = () => {
-  const { data, isLoading, hasNextPage, fetchNextPage } = useInfiniteQuery(
+  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(
     GraphqlRepository.getImagesQueryOptions({})
   );
-
-  console.log('data', data);
-  console.log('isLoading', isLoading);
-  console.log('hasNextPage', hasNextPage);
 
   const flattenedImages = data?.pages.flatMap(page => page.nodes) || [];
   console.log('flattenedImages', flattenedImages);
 
   return (
     <>
-      <div>Check</div>
-      {hasNextPage && <button onClick={() => fetchNextPage()}>Load more</button>}
+      <div className='text-red-300'>Check</div>
+      {hasNextPage && (
+        <button className='bg-blue-500' onClick={() => fetchNextPage()}>
+          Load more
+        </button>
+      )}
     </>
   );
 };
