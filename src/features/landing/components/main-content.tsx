ac7a401ed66833757cc28e151fe1039be627c895
pablo-aviles-prieto <pablo.aviles.prieto@gmail.com>
2024-12-27 00:35:53 +0100

    Add intersection observer and spinner for infinite images

--- src/features/landing/components/main-content.tsx
+++ src/features/landing/components/main-content.tsx
@@ -1,21 +1,32 @@
+import { useRef } from 'react';
+
 import { useInfiniteQuery } from '@tanstack/react-query';
 
 import { LayoutContentContainer } from '@/components/containers/content-layout';
+import { Spinner } from '@/components/loaders/spinner';
 import { useSearch } from '@/context/search-context';
 import { ImagesGrid } from '@/features/landing/components/images-grid';
+import useIntersection from '@/hooks/use-intersection';
 import GraphqlRepository from '@/services/graphql-repository';
 import { nonNullable } from '@/utils/non-nullable';
 
 export const MainContent = () => {
   const { searchWord } = useSearch();
+  const nextPageRef = useRef<HTMLDivElement>(null);
 
-  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(
+  const { data, hasNextPage, fetchNextPage, isLoading, isFetchingNextPage } = useInfiniteQuery(
     GraphqlRepository.getImagesQueryOptions({ title: searchWord || undefined })
   );
+  console.log('isLoading', isLoading);
+  console.log('isFetchingNextPage', isFetchingNextPage);
+
+  const intersectionCallback = (isIntersecting: boolean) => {
+    if (isIntersecting && hasNextPage) fetchNextPage();
+  };
+  useIntersection(nextPageRef, intersectionCallback);
 
   const flattenedImages = data?.pages.flatMap(page => page.nodes).filter(nonNullable) || [];
 
-  // TODO: Add the observe for infinite query
   // TODO: Style when the flattenedImages has 0 length
   // TODO: Add a placeholder when loading data
   return (
@@ -25,11 +36,12 @@ export const MainContent = () => {
         className='min-h-[calc(100vh-var(--header-height))] py-7 xl:py-14'
       >
         <ImagesGrid imagesData={flattenedImages} />
-        {hasNextPage && (
-          <button className='bg-blue-500' onClick={() => fetchNextPage()}>
-            Load more
-          </button>
+        {isFetchingNextPage && (
+          <div className='my-6 flex items-center justify-center'>
+            <Spinner />
+          </div>
         )}
+        {hasNextPage && <div ref={nextPageRef} />}
       </LayoutContentContainer>
     </main>
   );
