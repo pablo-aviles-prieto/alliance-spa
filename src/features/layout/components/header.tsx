958132ce57cd92bb29977d9ef71f2a61f36f8120
pablo-aviles-prieto <pablo.aviles.prieto@gmail.com>
2024-12-28 16:56:49 +0100

    Extract search input

--- src/features/layout/components/header.tsx
+++ src/features/layout/components/header.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
-import { SearchIcon } from '@/assets/icons/search';
 import { SammyLogo } from '@/assets/sammy-logo';
 import { LayoutContentContainer } from '@/components/containers/content-layout';
 import { useSearch } from '@/context/search-context';
+import { HeaderSearchInput } from '@/features/layout/components/search-input';
 import { useDebounce } from '@/hooks/use-debounce';
 
 export const LayoutHeader = () => {
@@ -24,24 +24,15 @@ export const LayoutHeader = () => {
   };
   const debouncedUpdate = useDebounce(debounceCallback);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchInput(e.target.value);
-    debouncedUpdate(e.target.value);
+  const handleChange = (value: string) => {
+    setSearchInput(value);
+    debouncedUpdate(value);
   };
 
   return (
     <LayoutContentContainer className='flex h-[var(--header-height)] flex-col items-start justify-between bg-primary py-6 sm:flex-row sm:items-center'>
       <SammyLogo />
-      {/* TODO: Export into his own component */}
-      <div className='mx-auto flex min-h-[30px] min-w-[270px] items-center justify-center gap-x-1 rounded-[20px] bg-[#F2F2F2] px-2 sm:mx-0'>
-        <SearchIcon className='size-5' />
-        <input
-          className='min-w-[220px] bg-inherit focus:outline-none'
-          placeholder={`You're looking for something?`}
-          value={searchInput}
-          onChange={handleChange}
-        />
-      </div>
+      <HeaderSearchInput value={searchInput} onChangeHandler={handleChange} />
     </LayoutContentContainer>
   );
 };
